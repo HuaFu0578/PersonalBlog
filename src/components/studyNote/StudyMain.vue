716a1aabe4fdf89781c5b66b12e7870b7bcaa333
LiuHuaifu <huafull0578@163.com>
2019-12-12 17:23:58 +0800

    study note modules first build

--- src/components/studyNote/StudyMain.vue
+++ src/components/studyNote/StudyMain.vue
@@ -3,13 +3,13 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-08 15:49:07
  * @LastEditors: your name
- * @LastEditTime: 2019-12-08 20:51:37
+ * @LastEditTime: 2019-12-12 16:29:59
  -->
 <template>
   <div class="study-wrap">
     <module-introduce :module="module" />
     <deadline-title head="本栏文章" #module>
-      <article-list :articleList="articleList" />
+      <article-list :articleList="articleList" :readArticle="readArticle" />
     </deadline-title>
   </div>
 </template>
@@ -22,12 +22,12 @@ export default {
     return {
       module: {
         name: "学习笔记",
-        img:require('@/assets/img/studynote.png'),
+        img: require("@/assets/img/studynote.png"),
         intro: "学习笔记模块主要用来记录平时学习过程的经验，心得！"
       }
     };
   },
-  props: ["articleList"],
+  props: ["articleList", "readArticle"],
   components: {
     ArticleList,
     DeadlineTitle,
--- src/views/StudyNote.vue
+++ src/views/StudyNote.vue
@@ -3,13 +3,14 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-07 15:43:55
  * @LastEditors: your name
- * @LastEditTime: 2019-12-08 20:57:55
+ * @LastEditTime: 2019-12-12 17:19:05
  -->
 <template>
   <div class="study-note">
     <div class="main">
       <body-block #module>
-        <study-main :articleList="articleList" />
+        <study-main :articleList="articleList" :readArticle="readArticle" />
+        <!-- <router-view ></router-view> -->
       </body-block>
     </div>
   </div>
@@ -17,71 +18,35 @@
 <script>
 import BodyBlock from "../components/BodyBlock";
 import StudyMain from "../components/studyNote/StudyMain";
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
-      articleList: [
-        {
-          title: "文章标题1",
-          img: "~@/assets/img/兄弟们有人闹事.gif",
-          content:
-            "阿萨德开了房监考老师大 打开疯狂 阿斯蒂芬会计法奥斯卡九分裤睡大觉焚枯食淡 开始打飞机 斯柯达复健科拉束带结发了卡戴珊解放路可接受的法兰克福免不了等方面阿SDK冷风机塑料袋咖啡机拉看束带结发克雷登斯荆防颗粒坚实的疯狂老板娘老师卡萨丁房间里看电视解放路看电视剧弗兰克日欧派膨润土的覆盖卡里古拉富可敌国圣诞快乐非建安独立思考等方面两方面算法 的酸辣粉快结束了开大加   阿市领导开房记录sad会计分录卡仕达解放路卡束带结发了看见没法拉盛",
-          date: "2019-12-8",
-          author: "Danker",
-          tags: "javascript",
-          introdution: "简介简介见阶简介简介奸恶简介简介简介奸恶你"
-        },
-        {
-          title: "文章标题2",
-          img: "~@/assets/img/兄弟们有人闹事.gif",
-          content: "阿萨德开了房监考老师大 打开疯狂 阿斯蒂芬会计法",
-          date: "2019-12-8",
-          author: "Danker",
-          tags: "javascript",
-          introdution: "简介简介见阶简介简介奸恶简介简介简介奸恶你"
-        },
-        {
-          title: "文章标题3",
-          img: "~@/assets/img/兄弟们有人闹事.gif",
-          content: "阿萨德开了房监考老师大 打开疯狂 阿斯蒂芬会计法",
-          date: "2019-12-8",
-          author: "Danker",
-          tags: "javascript",
-          introdution: "简介简介见阶简介简介奸恶简介简介简介奸恶你"
-        },
-        {
-          title: "文章标题4",
-          img: "~@/assets/img/兄弟们有人闹事.gif",
-          content: "阿萨德开了房监考老师大 打开疯狂 阿斯蒂芬会计法",
-          date: "2019-12-8",
-          author: "Danker",
-          tags: "javascript",
-          introdution: "简介简介见阶简介简介奸恶简介简介简介奸恶你"
-        },
-        {
-          title: "文章标题5",
-          img: "~@/assets/img/兄弟们有人闹事.gif",
-          content: "阿萨德开了房监考老师大 打开疯狂 阿斯蒂芬会计法",
-          date: "2019-12-8",
-          author: "Danker",
-          tags: "javascript",
-          introdution: "简介简介见阶简介简介奸恶简介简介简介奸恶你"
-        },
-        {
-          title: "文章标题6",
-          img: "~@/assets/img/兄弟们有人闹事.gif",
-          content: "阿萨德开了房监考老师大 打开疯狂 阿斯蒂芬会计法",
-          date: "2019-12-8",
-          author: "Danker",
-          tags: "javascript",
-          introdution: "简介简介见阶简介简介奸恶简介简介简介奸恶你"
-        }
-      ]
+      articleList: []
     };
+  },
+  computed: {
+    ...mapGetters(["siteConfig"])
   },
   components: {
     BodyBlock,
     StudyMain
+  },
+  created() {
+    this.$axios
+      .get(this.siteConfig.getArticleByType, {
+        params: { type: "学习笔记", page: 1, pageSize: 10 }
+      })
+      .then(res => {
+        let data = res.data.data;
+        this.articleList = data;
+      });
+  },
+  methods: {
+    readArticle(article) {
+      this.$store.commit("changeCurModule", "学习笔记");
+      this.$store.commit("setCurArticle", article);
+    }
   }
 };
 </script>
