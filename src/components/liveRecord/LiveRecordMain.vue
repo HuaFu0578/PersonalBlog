576be0861eb379e84e6ca362b295793147333b6b
LiuHuaifu <huafull0578@163.com>
2019-12-13 22:26:21 +0800

    liveRecord page has finished

---
+++ src/components/liveRecord/LiveRecordMain.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Description: file description
+ * @Author: LiuHuaifu
+ * @Date: 2019-12-08 21:03:11
+ * @LastEditors: your name
+ * @LastEditTime: 2019-12-13 15:50:43
+ -->
+<template>
+  <div class="study-wrap">
+    <module-introduce :module="module" />
+    <deadline-title head="本栏文章" #module>
+      <article-list :articleList="articleList" />
+    </deadline-title>
+  </div>
+</template>
+<script>
+import DeadlineTitle from "../common/DeadlineTitle";
+import ArticleList from "../common/article/ArticleList";
+import ModuleIntroduce from "../common/ModuleIntroduce";
+export default {
+  data() {
+    return {
+      module: {
+        name: "生活随记",
+        img: require("@/assets/img/liverecord.png"),
+        intro: "生活随记模块主要用来记录平时的点滴生活。随记，让生活更高效！"
+      }
+    };
+  },
+  props: ["articleList"],
+  components: {
+    ArticleList,
+    DeadlineTitle,
+    ModuleIntroduce
+  }
+};
+</script>
--- src/views/LiveRecord.vue
+++ src/views/LiveRecord.vue
@@ -3,54 +3,31 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-08 17:00:25
  * @LastEditors: your name
- * @LastEditTime: 2019-12-11 21:18:25
+ * @LastEditTime: 2019-12-13 13:31:05
  -->
 <template>
   <div class="live-record">
-    <div class="main">
-      <body-block #module>
-        <live-main :articleList="articleList" :readyArticle="readArticle" />
-      </body-block>
+    <header-bar />
+    <div class="wrapper">
+      <router-view></router-view>
     </div>
+    <footer-bar />
   </div>
 </template>
 <script>
-import BodyBlock from "../components/BodyBlock";
-import LiveMain from "../components/liveRecord/LiveMain";
+import HeaderBar from "../components/common/HeaderBar";
+import FooterBar from "../components/common/FooterBar";
 import { mapGetters } from "vuex";
 export default {
-  data() {
-    return {
-      articleList: []
-    };
-  },
-  computed: {
-    ...mapGetters(["siteConfig"])
-  },
   components: {
-    BodyBlock,
-    LiveMain
-  },
-  created() {
-    this.$axios
-      .get(this.siteConfig.getArticleByType, {
-        params: { type: "生活随记", page: 1, pageSize: 10 }
-      })
-      .then(res => {
-        let data = res.data.data;
-        this.articleList = data;
-      });
-  },
-  methods: {
-    readArticle(article) {
-      this.$store.commit("changeCurModule", "生活随记");
-      this.$store.commit("setCurArticle", article);
-    }
+    HeaderBar,
+    FooterBar
   }
 };
 </script>
 <style lang="scss" scoped>
-.live-record {
-  width: 100%;
+.wrapper {
+  width: 1170px;
+  margin: 20px auto;
 }
 </style>
