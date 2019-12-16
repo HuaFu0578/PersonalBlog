0dba590e50c926f75cb9dd89c596d71c209f03ce
LiuHuaifu <huafull0578@163.com>
2019-12-16 13:15:56 +0800

    question discuss articlelist api replace

--- src/components/questionDiscuss/QuestionDiscussMain.vue
+++ src/components/questionDiscuss/QuestionDiscussMain.vue
@@ -3,13 +3,13 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-08 20:59:37
  * @LastEditors: your name
- * @LastEditTime: 2019-12-13 15:50:03
+ * @LastEditTime: 2019-12-16 10:28:53
  -->
 <template>
   <div class="study-wrap">
     <module-introduce :module="module" />
     <deadline-title head="本栏文章" #module>
-      <article-list :articleList="articleList" />
+      <article-list :api="api" />
     </deadline-title>
   </div>
 </template>
@@ -17,6 +17,7 @@
 import DeadlineTitle from "../common/DeadlineTitle";
 import ArticleList from "../common/article/ArticleList";
 import ModuleIntroduce from "../common/ModuleIntroduce";
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
@@ -28,7 +29,19 @@ export default {
       }
     };
   },
-  props: ["articleList"],
+  computed: {
+    ...mapGetters(["siteConfig"]),
+    api() {
+      return {
+        getArticle: this.siteConfig.getArticleByType,
+        getArticleCount: this.siteConfig.getArticleCountByType,
+        params: {
+          getArticle: { type: "问题探讨" },
+          getArticleCount: { type: "问题探讨" }
+        }
+      };
+    }
+  },
   components: {
     ArticleList,
     DeadlineTitle,
--- src/views/subViews/QuestionDiscussWrap.vue
+++ src/views/subViews/QuestionDiscussWrap.vue
@@ -3,39 +3,20 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-12 21:16:44
  * @LastEditors: your name
- * @LastEditTime: 2019-12-13 15:49:45
+ * @LastEditTime: 2019-12-15 22:25:38
  -->
 <template>
   <body-block #module>
-    <question-discuss-main :articleList="articleList" />
+    <question-discuss-main />
   </body-block>
 </template>
 <script>
 import BodyBlock from "../../components/common/BodyBlock";
 import QuestionDiscussMain from "../../components/questionDiscuss/QuestionDiscussMain";
-import { mapGetters } from "vuex";
 export default {
-  data() {
-    return {
-      articleList: []
-    };
-  },
-  computed: {
-    ...mapGetters(["siteConfig"])
-  },
   components: {
     BodyBlock,
     QuestionDiscussMain
-  },
-  created() {
-    this.$axios
-      .get(this.siteConfig.getArticleByType, {
-        params: { type: "问题探讨", page: 1, pageSize: 10 }
-      })
-      .then(res => {
-        let data = res.data.data;
-        this.articleList = data;
-      });
   }
 };
 </script>
