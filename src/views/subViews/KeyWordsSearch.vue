314ba978d644a2627e25fb3b6daac1b72a01a108
LiuHuaifu <huafull0578@163.com>
2019-12-16 13:22:05 +0800

    search wrap modifyed

--- src/views/subViews/KeyWordsSearch.vue
+++ src/views/subViews/KeyWordsSearch.vue
@@ -3,55 +3,36 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-13 20:01:12
  * @LastEditors: your name
- * @LastEditTime: 2019-12-13 22:02:35
+ * @LastEditTime: 2019-12-16 10:49:27
  -->
 <template>
   <div class="keywords-show">
-    <search-show head="关键词搜索结果" :resultCount="resultCount" :articleList="articleList" />
+    <search-show head="关键词搜索结果" :api="api" />
   </div>
 </template>
 <script>
 import SearchShow from "../../components/common/SearchShow";
 import { mapGetters } from "vuex";
 export default {
-  data() {
-    return {
-      articleList: [],
-    };
-  },
   computed: {
     ...mapGetters(["siteConfig"]),
-    resultCount() {
-      return this.articleList.length;
+    api() {
+      return {
+        getArticle: this.siteConfig.getArticleListByKeyWords,
+        getArticleCount: this.siteConfig.getArticleCountByKeyWords,
+        params: {
+          getArticle: {
+            key_word: this.$route.params.key_word || this.$route.query.key_word
+          },
+          getArticleCount: {
+            key_word: this.$route.params.key_word || this.$route.query.key_word
+          }
+        }
+      };
     }
   },
   components: {
     SearchShow
-  },
-  methods: {
-    //通过标签id来获取文章列表
-    getArticlesByKeyWords(key_word, success) {
-      this.$axios
-        .get(this.siteConfig.getArticleListByKeyWords, {
-          params: { key_word: key_word, page: 1, pageSize: 10000 }
-        })
-        .then(res => {
-          let data = res.data.data;
-          this.articleList = data;
-          typeof success == "function" && success(res);
-        });
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      let key_word = to.params.key_word || to.query.key_word;
-      vm.getArticlesByKeyWords(key_word);
-    });
-  },
-  beforeRouteUpdate(to, from, next) {
-    let key_word = to.params.key_word || to.query.key_word;
-    this.getArticlesByKeyWords(key_word);
-    next();
   }
 };
 </script>
--- src/views/subViews/TagSearch.vue
+++ src/views/subViews/TagSearch.vue
@@ -3,56 +3,36 @@
  * @Author: LiuHuaifu
  * @Date: 2019-12-13 15:19:45
  * @LastEditors: your name
- * @LastEditTime: 2019-12-13 20:05:31
+ * @LastEditTime: 2019-12-16 12:54:03
  -->
 <template>
   <div class="tag-show">
-    <search-show head="标签搜索结果" :resultCount="resultCount" :articleList="articleList" />
+    <search-show head="标签搜索结果" :api="api" />
   </div>
 </template>
 <script>
 import SearchShow from "../../components/common/SearchShow";
 import { mapGetters } from "vuex";
 export default {
-  data() {
-    return {
-      articleList: []
-    };
-  },
   computed: {
     ...mapGetters(["siteConfig"]),
-
-    resultCount() {
-      return this.$route.params.count || this.$route.query.count || 0;
+    api() {
+      return {
+        getArticle: this.siteConfig.getArticleByTag,
+        getArticleCount: this.siteConfig.getArticleCountByTag,
+        params: {
+          getArticle: {
+            tag_id: this.$route.params.tag_id || this.$route.query.tag_id
+          },
+          getArticleCount: {
+            tag_id: this.$route.params.tag_id || this.$route.query.tag_id
+          }
+        }
+      };
     }
   },
   components: {
     SearchShow
-  },
-  methods: {
-    //通过标签id来获取文章列表
-    getArticlesByTagId(tag_id, success) {
-      this.$axios
-        .get(this.siteConfig.getArticleByTag, {
-          params: { tag_id: parseInt(tag_id), page: 1, pageSize: 10000 }
-        })
-        .then(res => {
-          let data = res.data.data;
-          this.articleList = data;
-          typeof success == "function" && success(res);
-        });
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      let tag_id = to.params.tag_id || to.query.tag_id;
-      vm.getArticlesByTagId(tag_id);
-    });
-  },
-  beforeRouteUpdate(to, from, next) {
-    let tag_id = to.params.tag_id || to.query.tag_id;
-    this.getArticlesByTagId(tag_id);
-    next();
   }
 };
 </script>
